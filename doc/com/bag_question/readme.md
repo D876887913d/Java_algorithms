35510d687610a1fccbc65de60e68ffaebefe8440
littlelite <3315149914@qq.com>
2023-10-01 02:18:13 +0800

    实现了二维数组一维优化，添加了混合背包问题的解决方案。

--- doc/com/bag_question/readme.md
+++ doc/com/bag_question/readme.md
@@ -3,10 +3,39 @@
 - [x] 完全背包问题
 - [x] 多重背包问题 - 无优化
 - [x] 多重背包问题 - 二进制优化 
-- [ ] 多重背包问题 - 单调队列优化
+- [x] 多重背包问题 - 单调队列优化
+- [ ] 混合背包问题
+- [ ] 二维费用的背包问题
+- [ ] 分组背包问题
+- [ ] 有依赖的背包问题
+- [ ] 背包问题求方案数
+- [ ] 背包问题求具体方案
 
 算法优化部分TODO：
-- [ ] 采用一维优化动态数组，提高动态数组的运行效率，并减少其空间消耗。
+- [ ] 拷贝数组法优化。
+- [ ] 滚动数组法优化。
+
+# 背包问题 二维基本状态转移
+
+```
+int [][] dp = new int [cnt+1][V+1];
+
+for(int i = 1; i <= cnt; i++){
+    for(int j = 1; j <= V; j++){
+        if(j < volumes[i-1]){
+            dp[i][j] = dp[i - 1][j];
+        }else{
+            dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j-volumes[i - 1]] + values[i - 1]);
+        }
+    }
+}
+
+System.out.println(dp[N][V]);
+```
+
+
+
+
 
 # 多重背包问题 - 二进制优化
 这个部分主要是在输入部分进行处理的，将多重背包问题转化为朴素背包问题，将背包的个数进行展开：
@@ -14,26 +43,31 @@
 // 主要代码如下
 int cnt = 0;
 
-        for(int i = 0 ;i < N; i++){
-            int vo, va, num;
-            vo = scanner.nextInt();
-            va = scanner.nextInt();
-            num = scanner.nextInt();
+for(int i = 0 ;i < N; i++){
+    int vo, va, num;
+    vo = scanner.nextInt();
+    va = scanner.nextInt();
+    num = scanner.nextInt();
 
-            int k = 1;
-            while(k <= num){
-                volums[cnt] = k * vo;
-                values[cnt] = k * va;
-                // 每分出去一组，原本的总数就要减少。
-                num -= k;
-                k *= 2;
-                cnt ++ ;
-            }
+    int k = 1;
+    while(k <= num){
+        volums[cnt] = k * vo;
+        values[cnt] = k * va;
+        // 每分出去一组，原本的总数就要减少。
+        num -= k;
+        k *= 2;
+        cnt ++ ;
+    }
 
-            if(num > 0){
-                volums[cnt] = num * vo;
-                values[cnt] = num * va;
-                cnt++;
-            }
-        }
+    if(num > 0){
+        volums[cnt] = num * vo;
+        values[cnt] = num * va;
+        cnt++;
+    }
+}
 ``` 
+
+# 滚动数组
+将原本的数组的维度转化为dp[2][V + 1]，能这样做的原因是：
+1. 二者之间具有的转移关系就只存在于两个相邻的元素上，跟其余的元素无关；
+2. 二者之间进行转移的过程中不会对其他元素产生对应的一些影响。
---
+++ src/com/bag/mix_bag_simple.java
@@ -0,0 +1,80 @@
+package com.bag;
+import java.util.Scanner;
+/*
+有N 种物品和一个容量是T的背包。
+物品一共有三类:
+    第一类物品只能用1次 (01背包)
+    第二类物品可以用无限次 (完全背包) ;
+    第三类物品最多只能用 s 次 (多重背包) ;
+每种体积是 vi，价值是 wi。
+求解将哪些物品装入背包，可使物品体积总和不超过背包容量，且价值总和最大。输出最大价值。
+输入格式
+    第一行两个整数，N，V，用空格隔开，分别表示物品种数和背包容积。
+    接下来有  行，每行三个整数 ,w,s，用空格隔开，分别表示第种物品的体积、价值和数量。
+        s = -1 表示第 种物品只能用1次
+        s = 0 表示第种物品可以用无限次:
+        s > 0表示第种物品可以使用 s次
+输出格式
+    输出一个整数，表示最大价值。
+
+4 5
+1 2 -1
+2 4 1
+3 4 0
+4 5 2
+ */
+// 整体的思路: 将所有的样本转化为 0 - 1 背包问题的格式：
+// 完全背包问题： 将其number设定为总体积除以该物品的体积向下取整
+// 获取好每种物品的个数后，采用二进制优化的方式将其转化为 0 - 1 背包问题
+
+public class mix_bag_simple {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int V = scanner.nextInt();
+
+        int volum, value, number;
+
+        int cnt = 0;
+
+        int[] volumes = new int[100010];
+        int[] values  = new int[100010];
+
+//      =============================二进制优化================================
+        for(int i = 0; i < N; i ++){
+            volum = scanner.nextInt();
+            value = scanner.nextInt();
+            number = scanner.nextInt();
+
+            if(number == -1)number = 1;
+            else if(number == 0)number = V / volum;
+
+            int k = 1;
+            while(k <= number){
+                volumes[cnt] = volum * number;
+                values[cnt] = value * number;
+                cnt += 1;
+                number -= k;
+                k *= 2;
+            }
+
+            if(number > 0){
+                volumes[cnt] = volum * number;
+                values[cnt] = value * number;
+                cnt += 1;
+            }
+        }
+//      ===========================二进制优化结束================================
+
+//      ===========================拷贝数组优化=================================
+        int[] dp = new int[V + 1];
+        for(int i = 1; i <= N; i ++){
+            for(int j = V; j >= volumes[i - 1]; j --){
+                dp[j] = Math.max(dp[j], dp[j - volumes[i - 1]] + values[i - 1]);
+            }
+        }
+        System.out.println(dp[V]);
+//      =========================拷贝数组优化结束=================================
+    }
+
+}
